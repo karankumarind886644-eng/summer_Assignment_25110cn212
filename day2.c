062383880c90b36f3de8c03a1416b4f146fb15d9
unknown <karankumarind886644@gmail.com>
2026-05-30 21:55:07 +0530

    added day3

--- day2.c
+++ day2.c
@@ -1,51 +1,53 @@
 //  write a program to find sum of digit of number
-// #include<stdio.h> 
-// int main(){
-//     int n,sum=0,remainder;
-//     printf("enter the number");
-//     scanf("%d",&n);
-//     while(n!=0){
-//         remainder = n%10;
-//         sum= sum +remainder;
-//         n = n/10;
-//     }
-//     printf("sum of digit =%d",sum);
-//     return 0;
-// }   
+
+
+#include<stdio.h> 
+int main(){
+    int n,sum=0,remainder;
+    printf("enter the number");
+    scanf("%d",&n);
+    while(n!=0){
+        remainder = n%10;
+        sum= sum +remainder;
+        n = n/10;
+    }
+    printf("sum of digit =%d",sum);
+    return 0;
+}   
 
 
 // write a program reverse of number
 
-// #include<stdio.h> 
-// int main(){
-//     int n,reverse=0,remainder;
-//     printf("enter the number");
-//     scanf("%d",&n);
-//     while(n!=0){
-//         remainder = n%10;
-//         reverse=    reverse*10+remainder;
-//         n = n/10;
-//     }
-//     printf("reverseof number =%d",reverse);
-//     return 0;
-// }
+#include<stdio.h> 
+int main(){
+    int n,reverse=0,remainder;
+    printf("enter the number");
+    scanf("%d",&n);
+    while(n!=0){
+        remainder = n%10;
+        reverse=    reverse*10+remainder;
+        n = n/10;
+    }
+    printf("reverseof number =%d",reverse);
+    return 0;
+}
 
 
 // write a program to find product of digit
 
-// #include<stdio.h> 
-// int main(){
-//     int n,product=1,remainder;
-//     printf("enter the number");
-//     scanf("%d",&n);
-//     while(n!=0){
-//         remainder = n%10;
-//         product = product*remainder;
-//         n = n/10;
-//     }
-//     printf("product of number =%d",product);
-//     return 0;
-// }
+#include<stdio.h> 
+int main(){
+    int n,product=1,remainder;
+    printf("enter the number");
+    scanf("%d",&n);
+    while(n!=0){
+        remainder = n%10;
+        product = product*remainder;
+        n = n/10;
+    }
+    printf("product of number =%d",product);
+    return 0;
+}
 
 // write a program to check whether a number is palindrome
 
