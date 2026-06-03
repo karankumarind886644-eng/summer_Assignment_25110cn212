60540866f93567148bc1000417217bc29d9d1b71
unknown <karankumarind886644@gmail.com>
2026-06-03 21:50:54 +0530

    Added day7

--- day6.c
+++ day6.c
@@ -1,81 +1,81 @@
 // write a program  to copnvert decimal to binary
 
 
-// #include <stdio.h>
+#include <stdio.h>
 
-// int main() {
-//     int decimal, binary[32], i = 0;
+int main() {
+    int decimal, binary[32], i = 0;
 
-//     printf("Enter a decimal number: ");
-//     scanf("%d", &decimal);
+    printf("Enter a decimal number: ");
+    scanf("%d", &decimal);
 
-//     if (decimal == 0) {
-//         printf("Binary = 0\n");
-//         return 0;
-//     }
+    if (decimal == 0) {
+        printf("Binary = 0\n");
+        return 0;
+    }
 
-//     while (decimal > 0) {
-//         binary[i] = decimal % 2;  // Store remainder
-//         decimal = decimal / 2;
-//         i++;
-//     }
+    while (decimal > 0) {
+        binary[i] = decimal % 2;  // Store remainder
+        decimal = decimal / 2;
+        i++;
+    }
 
-//     printf("Binary = ");
-//     for (int j = i - 1; j >= 0; j--) {
-//         printf("%d", binary[j]);
-//     }
+    printf("Binary = ");
+    for (int j = i - 1; j >= 0; j--) {
+        printf("%d", binary[j]);
+    }
 
-//     printf("\n");
-//     return 0;
-// }  
+    printf("\n");
+    return 0;
+}  
 
 
 //  write a program to convert binary  to decimal
 
 
-// #include <stdio.h>
-// #include <math.h>
+#include <stdio.h>
+#include <math.h>
 
-// int main() {
-//     long long binary;
-//     int decimal = 0, i = 0, remainder;
+int main() {
+    long long binary;
+    int decimal = 0, i = 0, remainder;
 
-//     printf("Enter a binary number: ");
-//     scanf("%lld", &binary);
+    printf("Enter a binary number: ");
+    scanf("%lld", &binary);
 
-//     while (binary != 0) {
-//         remainder = binary % 10;
-//         decimal += remainder * pow(2, i);
-//         binary /= 10;
-//         i++;
-//     }
+    while (binary != 0) {
+        remainder = binary % 10;
+        decimal += remainder * pow(2, i);
+        binary /= 10;
+        i++;
+    }
 
-//     printf("Decimal = %d\n", decimal);
+    printf("Decimal = %d\n", decimal);
 
-//     return 0;
-// }   
+    return 0;
+}   
 
 
 //  write a program to  count set bit in a number
 
 
-//  #include <stdio.h>
+ #include <stdio.h>
 
-// int main() {
-//     int n, count = 0;
+int main() {
+    int n, count = 0;
 
-//     printf("Enter a number: ");
-//     scanf("%d", &n);
+    printf("Enter a number: ");
+    scanf("%d", &n);
 
-//     while (n > 0) {
-//         count += n & 1;  // Check if last bit is 1
-//         n >>= 1;         // Right shift by 1
-//     }
+    while (n > 0) {
+        count += n & 1;  // Check if last bit is 1
+        n >>= 1;         // Right shift by 1
+    }
 
-//     printf("Number of set bits = %d\n", count);
+    printf("Number of set bits = %d\n", count);
 
-//     return 0;
-// }  
+    return 0;
+}  
 
 
 //  write a program to find x^n without power()
