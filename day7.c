ae34f62a39a77a3a11350dee221701c84507cb55
unknown <karankumarind886644@gmail.com>
2026-06-05 10:15:43 +0530

    Added day7

--- day7.c
+++ day7.c
@@ -29,13 +29,78 @@ int main()
 //  write  a program to recursive fibonacci
 
 
+#include <stdio.h>
+
+int factorial(int n)
+{
+    if (n == 0 || n == 1)
+        return 1;      // Base case
+
+    return n * factorial(n - 1); // Recursive call
+}
+
+int main()
+{
+    int n;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    printf("Factorial = %d", factorial(n));
+
+    return 0;
+}
+
 
 // write a program to recursive sum of digit
+
+
+#include <stdio.h>
+
+int sumDigits(int n)
+{
+    if (n == 0)
+        return 0;
+
+    return (n % 10) + sumDigits(n / 10);
+}
+
+int main()
+{
+    int num;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    printf("Sum of digits = %d\n", sumDigits(num));
+
+    return 0;
+}
 
 
 
 // write a program to recursive reverse number
 
+#include <stdio.h>
 
+int reverse(int n, int rev)
+{
+    if (n == 0)
+        return rev;
+
+    return reverse(n / 10, rev * 10 + n % 10);
+}
+
+int main()
+{
+    int num;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    printf("Reversed number = %d\n", reverse(num, 0));
+
+    return 0;
+}
 
 
