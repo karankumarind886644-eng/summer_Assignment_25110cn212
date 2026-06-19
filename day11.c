3bf12b5094b5a3fd42ee4a6d52bfb247a24306a4
Karan Rajak <karankumarind886644@gmail.com>
2026-06-19 11:37:50 +0530

    day11

--- day11.c
+++ day11.c
@@ -2,55 +2,114 @@
 
 #include <stdio.h>
 
-// Function to calculate sum of two numbers
-int sum(int a, int b) {
+// Function to find sum
+int sum(int a, int b)
+{
     return a + b;
 }
 
-int main() {
-    int num1, num2, result;
+int main()
+{
+    int x, y;
 
-    printf("Enter first number: ");
-    scanf("%d", &num1);
+    printf("Enter two numbers: ");
+    scanf("%d %d", &x, &y);
 
-    printf("Enter second number: ");
-    scanf("%d", &num2);
-
-    result = sum(num1, num2);
-
-    printf("Sum = %d\n", result);
+    printf("Sum = %d", sum(x, y));
 
     return 0;
-
 }
-include <stdio.h>
-unsigned long long factorial(int n) {
-    unsigned long long result = 1;
-    for(int i = 1; i <= n; i++) {
-        result *= i;
+
+
+// Function to Find Maximum Number
+
+#include <stdio.h>
+
+int maximum(int a, int b)
+{
+    if(a > b)
+        return a;
+    else
+        return b;
+}
+
+int main()
+{
+    int x, y;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &x, &y);
+
+    printf("Maximum = %d", maximum(x, y));
+
+    return 0;
+}
+
+
+
+//  Function to Check Prime Number
+
+#include <stdio.h>
+
+int isPrime(int n)
+{
+    int i;
+
+    if(n <= 1)
+        return 0;
+
+    for(i = 2; i <= n/2; i++)
+    {
+        if(n % i == 0)
+            return 0;
     }
-    return result;
+
+    return 1;
 }
 
-int main() {
+int main()
+{
     int num;
 
     printf("Enter a number: ");
     scanf("%d", &num);
 
-    if(num < 0) {
-        printf("Factorial is not defined for negative numbers.\n");
-    } else {
-        printf("Factorial of %d = %llu\n", num, factorial(num));
-    }
+    if(isPrime(num))
+        printf("Prime Number");
+    else
+        printf("Not Prime Number");
 
     return 0;
 }
 
 
+// Function to Find Factorial
 
+#include <stdio.h>
 
+int factorial(int n)
+{
+    int i, fact = 1;
 
+    for(i = 1; i <= n; i++)
+    {
+        fact = fact * i;
+    }
+
+    return fact;
+}
+
+int main()
+{
+    int num;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    printf("Factorial = %d", factorial(num));
+
+    return 0;
+}
 
 
 
