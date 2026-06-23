314d5ea002fc29c57babf50303681763730a31a1
unknown <karankumarind886644@gmail.com>
2026-06-23 21:38:05 +0530

    Added day24.c

--- day22.c
+++ day22.c
@@ -1,4 +1,219 @@
 
+
+// Write a program to Check palindrome string. 
+
+
+
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str[100], rev[100];
+    int i, len;
+
+    printf("Enter a string: ");
+    gets(str);
+
+    len = strlen(str);
+
+    for(i = 0; i < len; i++) {
+        rev[i] = str[len - i - 1];
+    }
+
+    rev[i] = '\0';
+
+    if(strcmp(str, rev) == 0)
+        printf("Palindrome String");
+    else
+        printf("Not a Palindrome String");
+
+    return 0;
+}
+
+// Write a program to Count words in a sentence
+
+#include <stdio.h>
+
+int main() {
+    char str[200];
+    int i = 0, words = 1;
+
+    printf("Enter a sentence: ");
+    fgets(str, sizeof(str), stdin);
+
+    // Count spaces
+    while (str[i] != '\0') {
+        if (str[i] == ' ')
+            words++;
+        i++;
+    }
+
+    printf("Number of words = %d\n", words);
+
+    return 0;
+}
+
+
+//  Write a program to Character frequency.  
+
+
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str[100], ch;
+    int i, count = 0;
+
+    printf("Enter a string: ");
+    gets(str);
+
+    printf("Enter character to find frequency: ");
+    scanf("%c", &ch);
+
+    for(i = 0; str[i] != '\0'; i++) {
+        if(str[i] == ch)
+            count++;
+    }
+
+    printf("Frequency of %c = %d", ch, count);
+
+    return 0;
+}
+
+
+// Write a program to Remove spaces from string
+
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str[100];
+    int i, j = 0;
+
+    printf("Enter a string: ");
+    gets(str);
+
+    for(i = 0; str[i] != '\0'; i++) {
+        if(str[i] != ' ') {
+            str[j] = str[i];
+            j++;
+        }
+    }
+
+    str[j] = '\0';
+
+    printf("String without spaces: %s", str);
+
+    return 0;
+}
+
+
+// Write a program to Check palindrome string. 
+
+
+
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str[100], rev[100];
+    int i, len;
+
+    printf("Enter a string: ");
+    gets(str);
+
+    len = strlen(str);
+
+    for(i = 0; i < len; i++) {
+        rev[i] = str[len - i - 1];
+    }
+
+    rev[i] = '\0';
+
+    if(strcmp(str, rev) == 0)
+        printf("Palindrome String");
+    else
+        printf("Not a Palindrome String");
+
+    return 0;
+}
+
+// Write a program to Count words in a sentence
+
+#include <stdio.h>
+
+int main() {
+    char str[200];
+    int i = 0, words = 1;
+
+    printf("Enter a sentence: ");
+    fgets(str, sizeof(str), stdin);
+
+    // Count spaces
+    while (str[i] != '\0') {
+        if (str[i] == ' ')
+            words++;
+        i++;
+    }
+
+    printf("Number of words = %d\n", words);
+
+    return 0;
+}
+
+
+//  Write a program to Character frequency.  
+
+
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str[100], ch;
+    int i, count = 0;
+
+    printf("Enter a string: ");
+    gets(str);
+
+    printf("Enter character to find frequency: ");
+    scanf("%c", &ch);
+
+    for(i = 0; str[i] != '\0'; i++) {
+        if(str[i] == ch)
+            count++;
+    }
+
+    printf("Frequency of %c = %d", ch, count);
+
+    return 0;
+}
+
+
+// Write a program to Remove spaces from string
+
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str[100];
+    int i, j = 0;
+
+    printf("Enter a string: ");
+    gets(str);
+
+    for(i = 0; str[i] != '\0'; i++) {
+        if(str[i] != ' ') {
+            str[j] = str[i];
+            j++;
+        }
+    }
+
+    str[j] = '\0';
+
+    printf("String without spaces: %s", str);
+
+    return 0;
+}
 // Write a program to Check palindrome string. 
 
 
---
+++ day24.c
@@ -0,0 +1,123 @@
+// /Write a program to Check string rotation. 
+
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str1[100], str2[100], temp[200];
+
+    printf("Enter first string: ");
+    scanf("%s", str1);
+
+    printf("Enter second string: ");
+    scanf("%s", str2);
+
+    if(strlen(str1) != strlen(str2)) {
+        printf("Not a rotation.\n");
+        return 0;
+    }
+
+    strcpy(temp, str1);
+    strcat(temp, str1);
+
+    if(strstr(temp, str2))
+        printf("String is a rotation.\n");
+    else
+        printf("String is not a rotation.\n");
+
+    return 0;
+}
+
+//  Write a program to Compress a string.
+
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str[100];
+
+    printf("Enter a string: ");
+    scanf("%s", str);
+
+    int count = 1;
+    int len = strlen(str);
+
+    for(int i = 0; i < len; i++) {
+        while(i < len - 1 && str[i] == str[i + 1]) {
+            count++;
+            i++;
+        }
+        printf("%c%d", str[i], count);
+        count = 1;
+    }
+
+    return 0;
+}
+
+//  Write a program to Find longest word. 
+
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str[200], longest[50], word[50];
+    int i = 0, j = 0, maxLen = 0;
+
+    printf("Enter a sentence: ");
+    fgets(str, sizeof(str), stdin);
+
+    while (1) {
+        if (str[i] != ' ' && str[i] != '\0' && str[i] != '\n') {
+            word[j++] = str[i];
+        } else {
+            word[j] = '\0';
+
+            if (j > maxLen) {
+                maxLen = j;
+                strcpy(longest, word);
+            }
+
+            j = 0;
+
+            if (str[i] == '\0' || str[i] == '\n')
+                break;
+        }
+        i++;
+    }
+
+    printf("Longest Word: %s\n", longest);
+
+    return 0;
+}
+
+//  Write a program to Remove duplicate character
+
+
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str[100];
+
+    printf("Enter a string: ");
+    scanf("%s", str);
+
+    int len = strlen(str);
+
+    for(int i = 0; i < len; i++) {
+        int flag = 0;
+
+        for(int j = 0; j < i; j++) {
+            if(str[i] == str[j]) {
+                flag = 1;
+                break;
+            }
+        }
+
+        if(flag == 0)
+            printf("%c", str[i]);
+    }
+
+    return 0;
+}
+
